60fd871050eb794c5fd427f72870433fd65b8e07
Yihao <zxas12584163@gmail.com>
2020-12-15 15:10:32 +0800

    add Day 01 and Day 13 code file

---
+++ Day-01/band-name-generator-start/main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Welcome to the band name generator")
+#2. Ask the user for the city that they grew up in.
+grewup_city = input("What's name of the city you grew up in?")
+#3. Ask the user for the name of a pet.
+pet_name = input("What's your pet's name?")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + grewup_city + " " + pet_name)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # python-1000-day
 It's python 100day code file from udeny
+
+
+
+
+
+https://thonny.org/
+
+
+
+http://pythontutor.com/visualize.html#mode=edit
